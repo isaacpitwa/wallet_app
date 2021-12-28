0d60cace929a1077647528586f33594c57ade291
Isaac Pitwa <isaacpitwa256@gmail.com>
2021-12-28 12:31:50 +0300

    Fix: code formating

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -32,54 +32,58 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0,
         actions: [
           IconButton(
-            icon:const Icon(Icons.notifications, color: Colors.blueGrey,),
-            onPressed: () {
-            },
+            icon: const Icon(
+              Icons.notifications,
+              color: Colors.blueGrey,
+            ),
+            onPressed: () {},
           ),
           IconButton(
-            icon:const  Icon(Icons.more_vert, color: Colors.blueGrey,),
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.blueGrey,
+            ),
             onPressed: () {},
           ),
         ],
         leading: IconButton(
-          icon: const Icon(Icons.menu, color: Colors.blueGrey,),
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.blueGrey,
+          ),
           onPressed: () {},
         ),
       ),
       body: Container(),
-      bottomNavigationBar:  SalomonBottomBar(
-      currentIndex: _currentIndex,
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.white,
-      margin: const EdgeInsets.only(bottom: 30, top: 10, right: 20, left: 20),
-      onTap: (index) {
-        setState(() {
-          _currentIndex = index;
-        });
-      },
-      items: [
-        SalomonBottomBarItem(
-            icon: const Icon(Icons.home),
-            title: const Text("Home"),
-            selectedColor: Colors.white
-        ),
-        SalomonBottomBarItem(
-            icon: const Icon(Icons.explore_outlined),
-            title: const Text("Likes"),
-            selectedColor: Colors.pink
-        ),
-        SalomonBottomBarItem(
-            icon: const Icon(Icons.data_usage),
-            title:const  Text("Search"),
-            selectedColor: Colors.orange
-        ),
-        SalomonBottomBarItem(
-            icon: const Icon(Icons.settings),
-            title: const Text("Profile"),
-            selectedColor: Colors.teal
-        )
-      ],
-    ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        margin: const EdgeInsets.only(bottom: 30, top: 10, right: 20, left: 20),
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          SalomonBottomBarItem(
+              icon: const Icon(Icons.home),
+              title: const Text("Home"),
+              selectedColor: Colors.white),
+          SalomonBottomBarItem(
+              icon: const Icon(Icons.explore_outlined),
+              title: const Text("Likes"),
+              selectedColor: Colors.pink),
+          SalomonBottomBarItem(
+              icon: const Icon(Icons.data_usage),
+              title: const Text("Search"),
+              selectedColor: Colors.orange),
+          SalomonBottomBarItem(
+              icon: const Icon(Icons.settings),
+              title: const Text("Profile"),
+              selectedColor: Colors.teal)
+        ],
+      ),
     );
   }
 }
