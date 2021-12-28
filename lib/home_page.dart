17c34f5ad8dd2aa1be75a23126d255f2304b9eeb
Isaac Pitwa <isaacpitwa256@gmail.com>
2021-12-28 12:27:40 +0300

    Feat: App bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xff161621),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications, color: Colors.blueGrey,),
+            onPressed: () {
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.blueGrey,),
+            onPressed: () {},
+          ),
+        ],
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.blueGrey,),
+          onPressed: () {},
+        ),
+      ),
+      body: Container(),
+    );
   }
 }
