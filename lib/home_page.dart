289a368eab2ae5cd09bc5e7253908739bce61213
Isaac Pitwa <isaacpitwa256@gmail.com>
2021-12-28 12:30:38 +0300

    Feat: bottom navigation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,30 +9,77 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _isLoaded = false;
+  int _currentIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+
+    // make _isLoaded true after 2 seconds
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        _isLoaded = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff161621),
+      backgroundColor: const Color(0xff161621),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
           IconButton(
-            icon: Icon(Icons.notifications, color: Colors.blueGrey,),
+            icon:const Icon(Icons.notifications, color: Colors.blueGrey,),
             onPressed: () {
             },
           ),
           IconButton(
-            icon: Icon(Icons.more_vert, color: Colors.blueGrey,),
+            icon:const  Icon(Icons.more_vert, color: Colors.blueGrey,),
             onPressed: () {},
           ),
         ],
         leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.blueGrey,),
+          icon: const Icon(Icons.menu, color: Colors.blueGrey,),
           onPressed: () {},
         ),
       ),
       body: Container(),
+      bottomNavigationBar:  SalomonBottomBar(
+      currentIndex: _currentIndex,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white,
+      margin: const EdgeInsets.only(bottom: 30, top: 10, right: 20, left: 20),
+      onTap: (index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      items: [
+        SalomonBottomBarItem(
+            icon: const Icon(Icons.home),
+            title: const Text("Home"),
+            selectedColor: Colors.white
+        ),
+        SalomonBottomBarItem(
+            icon: const Icon(Icons.explore_outlined),
+            title: const Text("Likes"),
+            selectedColor: Colors.pink
+        ),
+        SalomonBottomBarItem(
+            icon: const Icon(Icons.data_usage),
+            title:const  Text("Search"),
+            selectedColor: Colors.orange
+        ),
+        SalomonBottomBarItem(
+            icon: const Icon(Icons.settings),
+            title: const Text("Profile"),
+            selectedColor: Colors.teal
+        )
+      ],
+    ),
     );
   }
 }
